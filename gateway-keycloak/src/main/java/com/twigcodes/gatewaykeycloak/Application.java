d91ede8ddcd0ab38a86596dcc85548ec3e7b0033
wpcfan <peng.wang@outlook.com>
2018-12-18 01:51:27 +0800

    make keycloak adapter work

--- gateway-keycloak/src/main/java/com/twigcodes/gatewaykeycloak/Application.java
+++ gateway-keycloak/src/main/java/com/twigcodes/gatewaykeycloak/Application.java
@@ -2,12 +2,18 @@ package com.twigcodes.gatewaykeycloak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Application
  */
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@RefreshScope
 public class Application {
 
   public static void main(String[] args) {
